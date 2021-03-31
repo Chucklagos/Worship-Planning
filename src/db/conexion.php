ae072822864cea3763a955348278461f69fc5191
Chucklagos <chucklagos17@gmail.com>
2021-03-31 00:56:39 -0600

    feat: datos para la conexión online y local

--- src/db/conexion.php
+++ src/db/conexion.php
@@ -1,11 +1,20 @@
 <?php
 //session_start();
 
-$conexion = mysqli_connect(
+// Conexión en la nube
+/*$conexion = mysqli_connect(
   'vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   'drb6g4wswu2bfhgw',
   'h9dhysdg8hz6yygr',
   'asx4muqaxno0bdu3'
+);*/
+
+//Conexión de manera local
+$conexion = mysqli_connect(
+  'localhost',
+  'iglesia',
+  'iglesia',
+  'church'
 );
 
 if ($conexion) {
