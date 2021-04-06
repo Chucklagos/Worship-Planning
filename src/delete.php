11b1109c84560c86d4a82981e2c708d905ca0377
hectorriver <48781666+hectorriver@users.noreply.github.com>
2021-04-05 23:24:27 -0600

    FullCalendar

---
+++ src/delete.php
@@ -0,0 +1,18 @@
+<?php
+
+
+if(isset($_POST["id"]))
+{
+ $connect = new PDO('mysql:host=localhost;dbname=church', 'root', '');
+ $query = "
+ DELETE from events WHERE id=:id
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':id' => $_POST['id']
+  )
+ );
+}
+
+?>
