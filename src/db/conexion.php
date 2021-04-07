46c7c2ef1774efa40ff34f453e3c19f11c646c2a
Chucklagos <chucklagos17@gmail.com>
2021-04-06 23:14:16 -0600

    deploy

--- src/db/conexion.php
+++ src/db/conexion.php
@@ -2,21 +2,21 @@
 //session_start();
 
 // Conexión en la nube
-/*$conexion = mysqli_connect(
+$conexion = mysqli_connect(
   'vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   'drb6g4wswu2bfhgw',
   'h9dhysdg8hz6yygr',
   'asx4muqaxno0bdu3'
-);*/
+);
 
 //Conexión de manera local
-$conexion = mysqli_connect(
+/*$conexion = mysqli_connect(
   'localhost',
   'iglesia',
   'iglesia',
   'church'
 );
-
+*/
 if ($conexion) {
  // echo('Si hay conexion con la base de datos');
 }
