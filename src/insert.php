82db52fdfdca464377bc99fcaeaa807c5fb058e1
Chucklagos <chucklagos17@gmail.com>
2021-04-07 00:31:48 -0600

    conexión en localhost

--- src/insert.php
+++ src/insert.php
@@ -1,7 +1,8 @@
 <?php
 
 //$connect = new PDO('mysql:host=localhost;dbname=church', 'root', '');
-$connect = new PDO('mysql:host=vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=asx4muqaxno0bdu3', 'drb6g4wswu2bfhgw', 'h9dhysdg8hz6yygr');
+$connect = new PDO('mysql:host=localhost;dbname=church', 'iglesia', 'iglesia');
+//$connect = new PDO('mysql:host=vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=asx4muqaxno0bdu3', 'drb6g4wswu2bfhgw', 'h9dhysdg8hz6yygr');
 
 if(isset($_POST["title"]))
 {
