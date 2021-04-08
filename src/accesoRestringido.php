b9a696b5441fe467dcf46a71a443b02f06f792e7
Karen <karenmelendez_11@hotmail.com>
2021-04-07 21:45:39 -0600

    Pantalla acceso restringido

---
+++ src/accesoRestringido.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php include 'include/head.php'; ?>
+</head>
+
+<body class="m-0 row justify-content-center">
+    <div class="content" >
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-auto  col-12">
+                    <br>
+                    <br>
+                    <br>
+                        <center><img src="../assets/img/restringido.png" height="250px" width="250px"/></center>
+                        <br>
+                        <center><h1>Acceso Restringido</h1><center>
+                            <br>
+                        <button type="button" class="btn btn-outline-warning">Regresar</button>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
+</body>
+
+</html>
